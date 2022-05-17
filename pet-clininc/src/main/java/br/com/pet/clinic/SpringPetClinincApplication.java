f1120a5384e62010cb650be9bb7631299683f3c4
Eduardo SantAna <eduardosantana1313@gmail.com>
2022-05-16 22:00:10 -0300

    master initial

---
+++ pet-clininc/src/main/java/br/com/pet/clinic/SpringPetClinincApplication.java
@@ -0,0 +1,13 @@
+package br.com.pet.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinincApplication.class, args);
+	}
+
+}
