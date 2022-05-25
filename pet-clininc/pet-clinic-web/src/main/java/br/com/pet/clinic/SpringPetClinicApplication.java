fd86e0c19b6de26d9421ca7ffd7ba9d5462478f3
Eduardo SantAna <eduardosantana1313@gmail.com>
2022-05-25 20:40:57 -0300

    master fix pom.xml

--- pet-clininc/pet-clinic-web/src/main/java/br/com/pet/clinic/SpringPetClinicApplication.java
+++ pet-clininc/pet-clinic-web/src/main/java/br/com/pet/clinic/SpringPetClinicApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringPetClinicApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringPetClinicApplication.class, args);
 	}
 
