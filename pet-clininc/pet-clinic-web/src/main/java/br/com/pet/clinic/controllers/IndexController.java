1f600187b39fe85289b2c8ea9febe0482d718c41
Eduardo SantAna <eduardosantana1313@gmail.com>
2022-05-30 20:54:05 -0300

    master adding controllers and templates.

---
+++ pet-clininc/pet-clinic-web/src/main/java/br/com/pet/clinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package br.com.pet.clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
---
+++ pet-clininc/pet-clinic-web/src/main/java/br/com/pet/clinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package br.com.pet.clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+
+}
