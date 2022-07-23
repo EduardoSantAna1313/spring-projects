aceaaadaf29da5c1a584818e7ab7b0aac8c6bf40
Eduardo <eduardosantana1313@gmail.com>
2022-07-22 21:27:30 -0300

    add lombok to recipe app, adding jpa services and repositories

---
+++ recipe.app/src/test/java/br/com/edu/recipe/app/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package br.com.edu.recipe.app.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() throws Exception {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long id = 4l;
+        category.setId(id);
+        assertEquals(id, category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
